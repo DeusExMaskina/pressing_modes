6e483fb2a9176a6e67b065a6730b49415f208a56
Son Of Fjord <v4lkyn@yandex.ru>
2023-04-09 14:08:35 +0300

    Сделана адаптация под мобильные устройства. Добавлена иконка для лоадера

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
-from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.modalview import ModalView
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
 from kivy.config import Config
-from kivy.properties import ObjectProperty
-from kivy.lang import Builder
-
-import datetime as dt
-import json
-
-
 Config.set('graphics', 'resizable', 0)
-# Config.set('graphics', 'width', 400)
-# Config.set('graphics', 'height', 600)
-# Config.set('graphics', 'minimum_width', 200)
-# Config.set('graphics', 'minimum_height', 400)
+
+from kivy.core.window import Window
+Window.size = (400, 600)
+
+
+import json
+import datetime as dt
+from kivy.loader import Loader
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.modalview import ModalView
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+from kivy.app import App
+
 
 BTN_COLOR = [.39, .67, .82, 1]
 BTN_COLOR_CLOSE = [.96, .19, .19, 1]
 BTN_COLOR_CREATE = [.13, .78, 0, 1]
+Loader.loading_image = 'fanplast.png'
 
 
 class MainScreen(Screen):
@@ -37,7 +38,7 @@ class ScheduleModesScreen(Screen):
 
     def get_size_plate(self):
         """ Берет размер плит (целое число) и создает кнопки для составление режима прессования """
-        
+
         with open('plate.json', 'r') as file:
             self.data = json.load(file)
 
@@ -59,17 +60,17 @@ class ScheduleModesScreen(Screen):
         window.add_widget(gl)
         window.open()
 
-    
     def get_modes(self, *args):
         """ Берет из json время режимов прессования в минутах и выводит режимы в модальном окне """
 
-        window = ModalView(auto_dismiss=False)
+        window = ModalView(auto_dismiss=False, padding=10)
 
         with open('plate.json', 'r') as file:
             self.data = json.load(file)
 
         try:
-            time = dt.timedelta(hours=int(self.hour_input.text), minutes=int(self.minute_input.text))
+            time = dt.timedelta(hours=int(self.hour_input.text),
+                                minutes=int(self.minute_input.text))
         except ValueError:
             time = dt.timedelta(hours=0, minutes=0)
         mark = args[0].text
@@ -84,33 +85,39 @@ class ScheduleModesScreen(Screen):
 
         water_cooler_start = time
         if self.check_w.state == 'down':
-            time += dt.timedelta(minutes=self.data[mark]['winter_water_cooling'])
+            time += dt.timedelta(minutes=self.data[mark]
+                                 ['winter_water_cooling'])
         else:
-            time += dt.timedelta(minutes=self.data[mark]['summer_water_cooling'])
+            time += dt.timedelta(minutes=self.data[mark]
+                                 ['summer_water_cooling'])
 
         pressing_end = time
 
-        name_label = Label(text=f'Размер: {str(mark)}', font_size=24)
-        heat_label = Label(text=f'Прогрев: {str(heat_start)} - {str(pressing_start)}', font_size=24)
-        pressing_label = Label(text=f'Прессование: {str(pressing_start)} - {str(air_cooler_start)}', font_size=24)
-        air_cooler_label = Label(text=f'Воздушное охлаждение: {str(air_cooler_start)} - {str(water_cooler_start)}', font_size=24)
-        water_cooler_label = Label(text=f'Водяное охлаждение: {str(water_cooler_start)} - {str(pressing_end)}', font_size=24)
+        name_label = Label(
+            text=f'Размер: {str(mark)}', font_size=24, size_hint=(1, .2))
+        heat_label = Label(text=f'Прогрев:\n {str(heat_start)} - {str(pressing_start)}',
+                           font_size=24, size_hint=(1, .2), halign='center')
+        pressing_label = Label(
+            text=f'Прессование:\n {str(pressing_start)} - {str(air_cooler_start)}', font_size=24, size_hint=(1, .2), halign='center')
+        air_cooler_label = Label(
+            text=f'Воздушное охлаждение:\n {str(air_cooler_start)} - {str(water_cooler_start)}', font_size=24, size_hint=(1, .2), halign='center')
+        water_cooler_label = Label(
+            text=f'Водяное охлаждение:\n {str(water_cooler_start)} - {str(pressing_end)}', font_size=24, size_hint=(1, .2), halign='center')
 
-        gl = GridLayout(cols=1)
-        gl.add_widget(name_label)
-        gl.add_widget(heat_label)
-        gl.add_widget(pressing_label)
-        gl.add_widget(air_cooler_label)
-        gl.add_widget(water_cooler_label)
-        gl.add_widget(Button(
+        bl = BoxLayout(orientation='vertical', spacing=5)
+        bl.add_widget(name_label)
+        bl.add_widget(heat_label)
+        bl.add_widget(pressing_label)
+        bl.add_widget(air_cooler_label)
+        bl.add_widget(water_cooler_label)
+        bl.add_widget(Button(
             text='Назад',
             background_color=[.96, .19, .19, 1],
-            size_hint=[None, None],
-            size=[70, 30],
+            size_hint=[1, .1],
             on_press=window.dismiss
         ))
 
-        window.add_widget(gl)
+        window.add_widget(bl)
 
         window.open()
 
@@ -124,10 +131,13 @@ class ScheduleModesScreenManual(Screen):
     minute_input = ObjectProperty()
 
     def get_manual_mode(self):
-        window = ModalView(auto_dismiss=False)
+        """ Берет время введеное в поля input и расчитывает режимы пресования """
+
+        window = ModalView(auto_dismiss=False, padding=10)
 
         try:
-            time = dt.timedelta(hours=int(self.hour_input.text), minutes=int(self.minute_input.text))
+            time = dt.timedelta(hours=int(self.hour_input.text),
+                                minutes=int(self.minute_input.text))
             heat = dt.timedelta(minutes=int(self.inp_heat.text))
             pressing = dt.timedelta(minutes=int(self.inp_pressing.text))
             air = dt.timedelta(minutes=int(self.inp_air.text))
@@ -153,31 +163,32 @@ class ScheduleModesScreenManual(Screen):
         time += water
         pressing_end = time
 
-    
-        heat_label = Label(text=f'Прогрев: {str(heat_start)} - {str(pressing_start)}', font_size=24)
-        pressing_label = Label(text=f'Прессование: {str(pressing_start)} - {str(air_cooler_start)}', font_size=24)
-        air_cooler_label = Label(text=f'Воздушное охлаждение: {str(air_cooler_start)} - {str(water_cooler_start)}', font_size=24)
-        water_cooler_label = Label(text=f'Водяное охлаждение: {str(water_cooler_start)} - {str(pressing_end)}', font_size=24)
+        heat_label = Label(text=f'Прогрев:\n {str(heat_start)} - {str(pressing_start)}',
+                           font_size=24, size_hint=(1, .2), halign='center')
+        pressing_label = Label(
+            text=f'Прессование: {str(pressing_start)} - {str(air_cooler_start)}', font_size=24, size_hint=(1, .2), halign='center')
+        air_cooler_label = Label(
+            text=f'Воздушное охлаждение:\n {str(air_cooler_start)} - {str(water_cooler_start)}', font_size=24, size_hint=(1, .2), halign='center')
+        water_cooler_label = Label(
+            text=f'Водяное охлаждение:\n {str(water_cooler_start)} - {str(pressing_end)}', font_size=24, size_hint=(1, .2), halign='center')
 
-        gl = GridLayout(cols=1)
-        gl.add_widget(heat_label)
-        gl.add_widget(pressing_label)
-        gl.add_widget(air_cooler_label)
-        gl.add_widget(water_cooler_label)
-        gl.add_widget(Button(
+        bl = BoxLayout(orientation='vertical', spacing=5)
+        bl.add_widget(heat_label)
+        bl.add_widget(pressing_label)
+        bl.add_widget(air_cooler_label)
+        bl.add_widget(water_cooler_label)
+        bl.add_widget(Button(
             text='Назад',
             background_color=[.96, .19, .19, 1],
-            size_hint=[None, None],
-            size=[70, 30],
+            size_hint=[1, 0.1],
             on_press=window.dismiss
         ))
 
-        window.add_widget(gl)
+        window.add_widget(bl)
         window.open()
 
 
-
-kv=Builder.load_file('Press.kv')
+kv = Builder.load_file('Press.kv')
 
 
 class PressApp(App):
